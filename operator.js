564560817b51eb6d801e23924d1d14b748d5cdef
kanishkakathiresan13-star <kanishkakathiresan13@gmail.com>
2026-06-12 14:05:04 +0530

    operator

--- operator.js
+++ operator.js
@@ -6,3 +6,14 @@ console.log(a>0 &&b>0)
 console.log(a<0&&b<0)
 console.log(a<0||b<0)
 console.log(!a>0)
+//ynarypot
+var c=40
+console.log("ynary operator")
+console.log(c++)
+console.log(c--)
+console.log(++c)
+console.log(--c)
+ //ternary
+ console.log(ternary )
+ var result= (a>b) ? "a is greter" : "b is greter"
+ console.log(result)
