74e8b02514e42f12971a059c0b8e554a4032f09b
kanishkakathiresan13-star <kanishkakathiresan13@gmail.com>
2026-06-12 11:12:55 +0530

    opertor.js

---
+++ opertors.js
@@ -0,0 +1,24 @@
+
+var a =5
+var b =10
+console.log(a+b)//addition
+console.log(a-b)//subtraction
+console.log(a*b)//multiplication
+console.log(a/b)//divisoin
+console.log(a%b)//moduls
+console.log(a**b)//exepentiation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
