b05f5001c79e6a659562e981ddc797dd561d10bf
kanishkakathiresan13-star <kanishkakathiresan13@gmail.com>
2026-06-12 15:02:28 +0530

    database

---
+++ datatype.js
@@ -0,0 +1,32 @@
+console.log("welcome to priative datatype")
+
+//number
+console.log("dr number")
+var age=(19)
+console.log(age)
+
+//string
+console.log("dr string")
+var name =("kanii")
+
+//boolen
+console.log("dr boolen")
+var isstudent=true
+console.log(isstudent)
+//null
+var emptyvalue=null
+console.log(emptyvalue)
+//undefined
+var undenifed
+console.log(undefined)
+//object
+var person={
+    name:kanii
+    age:19
+}
+console.log(person.age)
+//array
+console.log("drarray")
+var number=(1,2,3,4,5)
+console.log(Array.length)
+console.log(numder[3])
